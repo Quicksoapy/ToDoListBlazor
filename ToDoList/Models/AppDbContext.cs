94e2b1ad92c43852b1bf92b1bddf791c01605239
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-09-27 19:48:45 +0200

    Initial Commit
    //finish code behind OnDone and OnDelete

---
+++ ToDoList/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions)
+        : base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
