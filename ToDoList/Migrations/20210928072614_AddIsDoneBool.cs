0bcd7fb0b564b815b5c826eaddddbdb59690a48e
Quicksoapy <73587977+Quicksoapy@users.noreply.github.com>
2021-09-28 10:32:51 +0200

    Added IsDoneBool to Tasks Table Done button and Delete buttons should now be functional
    //Make add task function

---
+++ ToDoList/Migrations/20210928072614_AddIsDoneBool.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class AddIsDoneBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDone",
+                table: "Tasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDone",
+                table: "Tasks");
+        }
+    }
+}
